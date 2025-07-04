514dfa15725b4f5672bfab0935763bf72057279d
lokeshbhai007 <lokeshmondal340@gmail.com>
2025-07-04 15:19:37 +0530

    favicon update

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,9 @@ import { ThemeProvider } from './components/Theme-provider'
 export const metadata = {
   title: 'TuneMate',
   description: 'Advanced AI-powered English learning and correction tool',
+  icons: {
+    icon: '/logo.png', // Favicon path from public folder
+  },
 }
 
 export default function RootLayout({ children }) {
